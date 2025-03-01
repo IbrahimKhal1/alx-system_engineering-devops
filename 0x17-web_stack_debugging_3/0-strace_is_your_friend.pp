8aa0cc73385dc18da0c8a0898a18e377fefa4a0d
Ibrahim <halidu.m1602411@st.futminna.edu.ng>
2025-03-01 08:57:16 -0800

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,19 +1,6 @@
-# This Puppet manifest fixes Apache 500 error for a WordPress site
+# Puppet manifest to fix Apache 500 error
 
-exec { 'install-php':
-  command => '/usr/bin/apt-get install -y php libapache2-mod-php',
-  path    => ['/usr/bin', '/usr/sbin'],
-  unless  => '/usr/bin/dpkg -l | grep php'
-}
-
-exec { 'set-permissions':
-  command => '/bin/chown -R www-data:www-data /var/www/html && /bin/chmod -R 755 /var/www/html',
-  path    => ['/bin', '/usr/bin'],
-  onlyif  => '/usr/bin/test -d /var/www/html'
-}
-
-service { 'apache2':
-  ensure  => 'running',
-  enable  => true,
-  require => Exec['install-php']
+exec { 'fix-wordpress':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
